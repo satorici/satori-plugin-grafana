133afd569dea6b9185a2b7b2c3b6f47ad7bc8a80
Mariano Carrazana <marianocarrazana@gmail.com>
2023-05-31 14:14:53 -0300

    Add autocomplete monitors list Remove logs Fix build

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,17 +1,35 @@
-import React, { ChangeEvent } from 'react';
-import { InlineField, Input, Select, SelectValue } from '@grafana/ui';
-import { QueryEditorProps } from '@grafana/data';
+import React from 'react';
+import { InlineField, AsyncSelect, Select, SelectValue } from '@grafana/ui';
+import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { DataSource } from '../datasource';
 import { MyDataSourceOptions, MyQuery } from '../types';
+import { getBackendSrv } from '@grafana/runtime';
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
-export function QueryEditor({ query, onChange, onRunQuery }: Props) {
-  const onIdChange = (event: ChangeEvent<HTMLInputElement>) => {
-    console.log('q:', query);
-    onChange({ ...query, objId: event.target.value });
+export function QueryEditor({ query, onChange, onRunQuery, datasource }: Props) {
+  const proxy = datasource.proxy_url;
+
+  const loadAsyncOptions = () => {
+    // Load monitors list
+    return new Promise<Array<SelectableValue<string>>>((resolve) => {
+      getBackendSrv()
+        .datasourceRequest({
+          method: 'GET',
+          url: `${proxy}/monitors`,
+        })
+        .then((res) => {
+          const monitor_list = res.data.list.map((e: any) => ({ label: e.id, value: e.id, description: e.name }));
+          resolve(monitor_list);
+        });
+    });
+  };
+
+  const onIdChange = (value: SelectValue<any>) => {
+    onChange({ ...query, objId: value.value });
     onRunQuery();
   };
+
   const onTypeChange = (value: SelectValue<any>) => {
     onChange({ ...query, objType: value.value });
     onRunQuery();
@@ -22,19 +40,14 @@ export function QueryEditor({ query, onChange, onRunQuery }: Props) {
     { label: 'Monitors', value: 'monitors' },
     { label: 'Reports', value: 'reports', description: 'list all reports' },
   ];
-  console.log('obj:', objId);
   return (
     <div className="gf-form">
       <InlineField label="Type" labelWidth={6}>
         <Select options={options} onChange={onTypeChange} width={30} value={objType} />
       </InlineField>
-      <InlineField disabled={objType === 'reports'} label={objType ? capitalize(objType.slice(0, -1)) + ' ID' : ''}>
-        <Input onChange={onIdChange} value={objId} />
+      <InlineField disabled={objType !== 'monitors'} label="Monitor ID">
+        <AsyncSelect loadOptions={loadAsyncOptions} defaultOptions onChange={onIdChange} width={30} value={objId} />
       </InlineField>
     </div>
   );
-}
-
-function capitalize(str: string): string {
-  return [...str][0].toUpperCase() + str.slice(1);
 }
--- src/datasource.ts
+++ src/datasource.ts
@@ -15,23 +15,18 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   proxy_url?: string;
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
-    console.log('settings:', instanceSettings.jsonData);
-    const satori_url = 'https://api.satori-ci.com';
     this.proxy_url = instanceSettings.url;
   }
 
   async apiRequest(query: MyQuery) {
-    console.log('apiRequest:', query);
     if (query.objId === undefined || query.objType === 'reports') {
       query.objId = '';
     }
     const url = `${this.proxy_url}/${query.objType}/${query.objId}`;
-    console.log(url);
     const result = await getBackendSrv().datasourceRequest({
       method: 'GET',
       url: url,
     });
-    console.log('result:', result);
     let reports = [];
     if (query.objType === 'monitors') {
       reports = result.data.reports;
@@ -42,10 +37,8 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   }
 
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
-    console.log('options:', options);
     const promises = options.targets.map((query) => {
       return this.apiRequest(query).then((reports) => {
-        console.info('res:', reports);
         const frame = new MutableDataFrame({
           refId: query.refId,
           meta: {
@@ -62,7 +55,6 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
         });
 
         reports.forEach((report: any) => {
-          console.log('report', report);
           frame.appendRow([
             Date.parse(report.created),
             report.fails,
@@ -72,15 +64,10 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
             report.comments,
           ]);
         });
-        console.log('frame:', frame);
         return frame;
       });
     });
-    console.log('promises:', promises);
-    return Promise.all(promises).then((data) => {
-      console.log('data:', data);
-      return { data };
-    });
+    return Promise.all(promises).then((data) => ({ data }));
   }
 
   async testDatasource() {
